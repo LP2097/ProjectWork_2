4ea76638fbf99763b3060cab0073c12b63f5b8f3
Riccardo Feletig <riccardo.feletig@tecnicosuperiorekennedy.it>
2019-05-15 11:27:51 +0200

    Aggiunte icone

--- app/src/main/java/thunderbytes/com/formulanews/Fragments/PilotsRanking.java
+++ app/src/main/java/thunderbytes/com/formulanews/Fragments/PilotsRanking.java
@@ -16,6 +16,6 @@ public class PilotsRanking extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calendar_layout, null);
+        return inflater.inflate(R.layout.activity_calendar, null);
     }
 }
