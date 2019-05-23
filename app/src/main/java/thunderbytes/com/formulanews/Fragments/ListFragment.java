0cc014c7a6aa46c7271b90e12d18527f4cce5ff4
Riccardo Feletig <riccardo.feletig@tecnicosuperiorekennedy.it>
2019-05-23 15:27:58 +0200

    Sistemata la grafica

--- app/src/main/java/thunderbytes/com/formulanews/Fragments/ListFragment.java
+++ app/src/main/java/thunderbytes/com/formulanews/Fragments/ListFragment.java
@@ -13,17 +13,21 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import thunderbytes.com.formulanews.Models.Race;
 import thunderbytes.com.formulanews.R;
 
 public class ListFragment extends Fragment {
     public static final String ID = "id";
     private int fragmentId;
+    private Race race;
     ListView listView;
     TextView title;
     ImageButton infoBtn;
 
-    String fakeName[] = {"Valtteri Bottas", "Lewis Hamilton" , "Sebastian Vettel", "Max verstappen"};
-    String fakeScore[] = {"87", "86", "52", "51"};
+    String fakePosition[] = {"1", "2" , "3", "4","5"};
+    String fakeFirstName[] = {"Lewis", "Valteri", "Max", "Sebastian","Charles"};
+    String fakeLastName[] = {"Hamilton", "Bottas", "Verstappen", "Vettel", "Leclerc"};
+    String fakePoints[] = {"112 PTS", "105 PTS", "66 PTS", "66 PTS", "66 PTS"};
 
     public ListFragment() { }
 
@@ -41,7 +45,7 @@ public class ListFragment extends Fragment {
         listView = (ListView)vView.findViewById(R.id.listView);
         fragmentId = getArguments().getInt(ID);
 
-        MyAdapter adapter = new MyAdapter(getContext(), fakeName, fakeScore);
+        MyAdapter adapter = new MyAdapter(getContext(), fakePosition, fakeFirstName, fakeLastName, fakePoints);
         listView.setAdapter(adapter);
         return vView;
     }
@@ -49,14 +53,18 @@ public class ListFragment extends Fragment {
 
     class MyAdapter extends ArrayAdapter<String> {
         Context context;
-        String rName[];
-        String rDate[];
+        String rankPosition[];
+        String upText[];
+        String downText[];
+        String points[];
 
-        MyAdapter(Context c, String name[], String date[]) {
-            super(c, R.layout.list_cell_layout, R.id.textLeft, name);
+        MyAdapter(Context c, String rankPosition[], String upName[], String downName[], String points[]) {
+            super(c, R.layout.list_cell_layout, R.id.position, rankPosition);
             this.context = c;
-            this.rName = name;
-            this.rDate = date;
+            this.rankPosition = rankPosition;
+            this.upText = upName;
+            this.downText = downName;
+            this.points = points;
         }
 
         @NonNull
@@ -64,36 +72,37 @@ public class ListFragment extends Fragment {
         public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater layoutInflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View cell = layoutInflater.inflate(R.layout.list_cell_layout, parent, false);
-            TextView textLeft = cell.findViewById(R.id.textLeft);
-            TextView textRight = cell.findViewById(R.id.textRight);
-            ImageButton infoBtn = cell.findViewById(R.id.infoBtn);
+            TextView Position = cell.findViewById(R.id.position);
+            TextView UpText = cell.findViewById(R.id.upText);
+            TextView DownText = cell.findViewById(R.id.downText);
+            TextView Points = cell.findViewById(R.id.points);
 
             switch (fragmentId) {
                 case 0:
                     title.setText("Calendario");
-                    infoBtn.setVisibility(View.VISIBLE);
+                    //infoBtn.setVisibility(View.VISIBLE);
                     break;
 
                 case 1:
                     title.setText("Classifica piloti");
-                    infoBtn.setVisibility(View.INVISIBLE);
+                    //infoBtn.setVisibility(View.INVISIBLE);
                     break;
 
                 case 2:
                     title.setText("Classifica costruttori");
-                    infoBtn.setVisibility(View.INVISIBLE);
+                    //infoBtn.setVisibility(View.INVISIBLE);
                     break;
             }
-            textLeft.setText(rName[position]);
-            textRight.setText(rDate[position]);
+            Position.setText(rankPosition[position]);
+            UpText.setText(upText[position]);
+            DownText.setText(downText[position]);
+            Points.setText(points[position]);
 
-            infoBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.d("API", rName[position]);
-                    mListener.onItemValue(rName[position]);
-                }
-            });
+            //infoBtn.setOnClickListener(new View.OnClickListener() {
+            //    @Override
+            //    public void onClick(View v) {
+            //    }
+            //});
 
             return cell;
         }
