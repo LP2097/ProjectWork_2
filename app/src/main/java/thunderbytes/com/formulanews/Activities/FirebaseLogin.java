57328701de15009b4af8a33ce6d976490278abec
Giulio Pretis <giuliopretis@gmail.com>
2019-06-12 17:24:20 +0200

    Aggiunto sign in e sign up con usermail e password

---
+++ app/src/main/java/thunderbytes/com/formulanews/Activities/FirebaseLogin.java
@@ -0,0 +1,6 @@
+package thunderbytes.com.formulanews.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class FirebaseLogin extends AppCompatActivity {
+}
---
+++ app/src/main/java/thunderbytes/com/formulanews/Activities/FirebaseRegister.java
@@ -0,0 +1,6 @@
+package thunderbytes.com.formulanews.Activities;
+
+import android.support.v4.app.ActivityCompat;
+
+public class FirebaseRegister extends ActivityCompat {
+}
